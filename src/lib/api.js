34c6c0338f92b371b79e567546eece14f183daa8
NightHawkNB <nipunbathiya1256@outlook.com>
2025-04-06 23:18:03 +0530

    [UPDATE] Base url update

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: `https://aidf-horizone-backend-nipun.onrender.com/api/`,
+    baseUrl: `https://aidf-horizone-backend-nipunbathiya.onrender.com/api/`,
     prepareHeaders: async (headers, { getState }) => {
       const token = await window?.Clerk?.session?.getToken();
 
